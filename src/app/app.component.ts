3adbaad23fec5e9681d6e68d255ea58eaa6bbc32
AsNoFran <fdehaies59@gmail.com>
2022-12-09 09:47:26 +0100

    final quest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Output';
+  onomatopoeiaList: string[] = [];
+
+  onReceiveNewOnomatopia(event: string): void {
+    this.onomatopoeiaList.push(event);
+  }
 }
